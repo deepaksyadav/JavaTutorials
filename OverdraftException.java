978b0faafe4ec66a62a9377b04f41dedc142779a
deepaksyadav <deepaksyadav@users.noreply.github.com>
2025-08-23 14:59:45 +0530

    Update OverdraftException.java

--- OverdraftException.java
+++ OverdraftException.java
@@ -1,6 +1,8 @@
 // Custom exception class
+// used in ThrowThrowsDemo.java
 public class OverdraftException extends Exception {
     public OverdraftException(String message) {
         super(message);
     }
 }
+
