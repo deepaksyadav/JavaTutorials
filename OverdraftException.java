1e2723475a41488279c817ffe8f8f1be66f0fd20
deepaksyadav <deepaksyadav@users.noreply.github.com>
2025-08-23 14:56:42 +0530

    Add files via upload

---
+++ OverdraftException.java
@@ -0,0 +1,6 @@
+// Custom exception class
+public class OverdraftException extends Exception {
+    public OverdraftException(String message) {
+        super(message);
+    }
+}
---
+++ ThrowThrowsDemo.java
@@ -0,0 +1,34 @@
+// A simple BankAccount class
+class BankAccount {
+    private double balance;
+
+    public BankAccount(double balance) {
+        this.balance = balance;
+    }
+
+    // Method to withdraw money
+    public void withdraw(double amount) throws OverdraftException {
+        if (amount > balance) {
+            throw new OverdraftException(
+                    "Attempt to withdraw Rs" + amount + " with balance of Rs" + balance + "."
+            );
+        } else {
+            balance -= amount;
+            System.out.println("Withdrawal successful. New balance: Rs" + balance);
+        }
+    }
+}
+
+// Main class to test
+public class ThrowThrowsDemo {
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount(50);
+
+        try {
+            account.withdraw(100);  // Will throw exception
+        } catch (OverdraftException e) {
+            System.out.println("Exception caught: " + e.getMessage());
+        }
+    }
+}
+
